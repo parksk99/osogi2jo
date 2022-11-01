71d868402816cb8a827b8ac01a44fd0e7dc3b156
seungmin Ryu <ojysep9987@naver.com>
2022-11-01 12:11:17 +0900

    Feat: 크롤링 성능 개선을 위한 Thread Pool 세팅

---
+++ server/opensource/src/main/java/com/opensource/opensource/config/AsyncConfig.java
@@ -0,0 +1,28 @@
+package com.opensource.opensource.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    private static int CORE_POOL_SIZE = 15;
+    private static int MAX_POOL_SIZE = 25;
+    private static int QUEUE_CAPACITY = 10;
+    private static String THREAD_NAME_PREFIX = "async-task";
+
+    @Bean
+    public Executor asyncTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        executor.initialize();
+        return executor;
+    }
+}
