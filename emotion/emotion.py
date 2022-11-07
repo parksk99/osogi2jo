f7e036453063f1be48726d9417f7d07ff3000c0c
parksk99 <qkrtjdrb99@naver.com>
2022-11-07 20:10:34 +0900

    빠진 knusl 추가

--- emotion/emotion.py
+++ emotion/emotion.py
@@ -1,16 +1,20 @@
 import json
 
 class Emotion():
+	# def __init__(self):
+	# 	with open('knusl/data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
+	# 		self.data = json.load(f)
+
 	def data_list(wordname):	
-		with open('emotion/knusl/data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
-			data = json.load(f)
 		result = ['None','None']	
+	 	with open('knusl/data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
+	 		data = json.load(f)
 		for i in range(0, len(data)):
 			if data[i]['word'] == wordname:
 				result.pop()
 				result.pop()
 				result.append(data[i]['word_root'])
-				result.append(data[i]['polarity'])	
+				result.append(data[i]['polarity'])
 		
 		r_word = result[0]
 		s_word = result[1]
--- emotion/main.py
+++ emotion/main.py
@@ -5,7 +5,7 @@ from word import Word
 keyword_detector = Word
 emotion_detector = Emotion
 
-novel_path = 'emotion/novel.txt'
+novel_path = 'novel.txt'
 
 keywords,rank = keyword_detector.get_word_from_novel(novel_path)  # 소설에서 단어 읽어들이기
 
@@ -16,7 +16,7 @@ print('길이 : ',len(keywords))
 start = time.time()
 for wordname, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:30]:
     wordname = wordname.strip(" ")		
-    word, emotion = emotion_detector.data_list(wordname) # 읽어들인 단어의 감정 분석
+    word, emotion = emotion_detector.data_list(wordname =  wordname) # 읽어들인 단어의 감정 분석
     if emotion != 'None':
         emotion_value = r*int(emotion)
         emotional_word.append((wordname,emotion_value))
