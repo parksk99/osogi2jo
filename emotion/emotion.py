8133344f1635cbfddc13fcd4d7b9611a91c242da
parksk99 <qkrtjdrb99@naver.com>
2022-11-07 21:03:15 +0900

    간단한 오류 제거

--- emotion/emotion.py
+++ emotion/emotion.py
@@ -1,22 +1,18 @@
 import json
 
 class Emotion():
-	# def __init__(self):
-	# 	with open('knusl/data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
-	# 		self.data = json.load(f)
-
-	def data_list(wordname):	
-		result = ['None','None']	
-	 	with open('knusl/data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
-	 		data = json.load(f)
-		for i in range(0, len(data)):
-			if data[i]['word'] == wordname:
-				result.pop()
-				result.pop()
-				result.append(data[i]['word_root'])
-				result.append(data[i]['polarity'])
-		
-		r_word = result[0]
-		s_word = result[1]
-		
-		return r_word, s_word
+   def data_list(wordname):   
+      with open('knusl/data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
+         data = json.load(f)
+      result = ['None','None']
+      for i in range(0, len(data)):
+         if data[i]['word'] == wordname:
+            result.pop()
+            result.pop()
+            result.append(data[i]['word_root'])
+            result.append(data[i]['polarity'])   
+      
+      r_word = result[0]
+      s_word = result[1]
+      
+      return r_word, s_word
