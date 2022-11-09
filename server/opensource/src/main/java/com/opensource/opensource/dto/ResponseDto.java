f8ecd473eeb326c5919ef32fa3889a01293a45d3
seungmin Ryu <ojysep9987@naver.com>
2022-11-09 14:23:37 +0900

    Feat: Django 서버와 통신을 위한 DTO

---
+++ server/opensource/src/main/java/com/opensource/opensource/dto/ResponseDto.java
@@ -0,0 +1,16 @@
+package com.opensource.opensource.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto {
+    private String result;
+
+//    public static class Content{
+//        private String result;
+//    }
+}
