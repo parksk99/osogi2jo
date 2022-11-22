3cfb4e4ede975285298a4c216839d95e8fe33956
seungmin Ryu <ojysep9987@naver.com>
2022-11-23 02:05:57 +0900

    Fix: API -> GET방식으로 변경 및 requestParam 추가

--- server/opensource/src/main/java/com/opensource/opensource/controller/ApiController.java
+++ server/opensource/src/main/java/com/opensource/opensource/controller/ApiController.java
@@ -1,12 +1,14 @@
 package com.opensource.opensource.controller;
 
-import com.opensource.opensource.dto.RequestDto;
+import com.opensource.opensource.dto.VideoInfoDto;
 import com.opensource.opensource.utils.RequestTextAnalaysis;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +17,9 @@ public class ApiController {
     private final RequestTextAnalaysis requestTextAnalaysis;
 
     @SneakyThrows
-    @PostMapping("/django")
-    public Object requestResult(@RequestBody RequestDto novel){
-        System.out.println(novel.getNovel());
-        return requestTextAnalaysis.useResTemplate(novel.getNovel());
+    @GetMapping("/novel")
+    public ArrayList<VideoInfoDto> requestResult(@RequestParam String novelUrl){
+        System.out.println(novelUrl);
+        return requestTextAnalaysis.useResTemplate(novelUrl);
     }
 }
