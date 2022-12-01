1f8fa6374f8f4879d527de327cc2b32a4a3435df
Daehyun Kwon <neoskyclad@naver.com>
2022-12-01 11:27:32 +0900

    문단 단위 정하기랑 main.py 합치기

--- emotion/main.py
+++ emotion/main.py
@@ -6,26 +6,50 @@ from word import Word
 keyword_detector = Word()
 emotion_detector = Emotion()
 
-# max_unit = 150      #문장 분석 수 / 150이 적당한 크기 같아서 임의로 설정함
-
-# start_pos = 0       #소설 시작 위치
-# end_pos = start_pos + max_unit       #소설 끝날 위치 = 시작 + 분석 수
-
-# default_min_count = 3        #키워드 최소 빈도수
-
 #파일에서 읽기
-novel_path = 'novel2.txt'    #5000줄로 늘림
+novel_path = 'novel3.txt'    #5000줄로 늘림
 novel= open(novel_path, 'r', encoding = 'utf-8').read()
 
+unit_array = []
 result_array = []
 
 text_reader = TextReader(novel)
+
+maxCount = 12
+bestIndex = 0
+count = [0 for i in range(maxCount)]
+
+with open(novel_path, 'r', encoding = 'utf-8') as novel_file:   #문단 단위 정하기
+        novel = novel_file.readlines()
+        for i in range(0,maxCount):
+            unit_length = (i+1)*100 #소설 문단 단위 100씩 증가
+            for j in range(0,int(5000/unit_length)):
+                keywords, rank = keyword_detector.get_word_from_novel(novel[j*unit_length:(j+1)*(unit_length)],2)
+                if(keywords is None):
+                    print(unit_length,'keyword is not detected')
+                    continue
+                for wordname, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:30]:
+                    wordname = wordname.strip(" ")	
+                    tmp, emotion = emotion_detector.data_list(wordname =  wordname)
+                    if emotion != 'None':
+                        count[i] = count[i]+1
+                # count[i] = count[i]+len(keywords)
+            count[i] = count[i] / (5000/unit_length)
+            unit_array.append(count[i])
+            if i == 0:
+                bestCount = count[i]
+                bestIndex = i
+            elif count[i] > bestCount:
+                bestCount = count[i]
+                bestIndex = i
+
+unit_length = (bestIndex+1)*100
+
 while(True):
     texts = text_reader.read()
     if texts is None:
         break
 
-    unit_length = int(text_reader.novel_len/5)
     emotional_word = []
     for i in range(0, 5):
         tmp = texts[unit_length*i : unit_length*(i+1)]
@@ -56,9 +80,10 @@ while(True):
                #     result_array.append((wordname,(text_reader.readsentence/text_reader.novel_len)*100))
     if(len(emotional_word)!=0):
         max_word = max(emotional_word, key = lambda x : x[1])
-        result_array.append((max_word[0],max_word[2]))
-
-    
+        # result_array.append((max_word[0],max_word[2]))
+        result_array.append((max_word))
+        # max_word = max(emotional_word, key = lambda x : x[1])
+        # result_array.append((max_word[0],max_word[2]))
 
     # start_pos = end_pos
     # end_pos = start_pos + max_unit
@@ -67,8 +92,7 @@ while(True):
     # print('감정 분석 소요 시간 : ',end-start)
     # print('감정 총합 : ',emotion_sum)
     # print(emotional_word)
-    # print('============================================================================================================')
-        
+    # print('============================================================================================================')    
 print('result : ',result_array)
     
 
