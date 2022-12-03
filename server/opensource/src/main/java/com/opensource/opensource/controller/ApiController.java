4473661489cff0737c47e9a61d8d5a8ce8a2e3c3
seungmin Ryu <ojysep9987@naver.com>
2022-12-03 18:14:08 +0900

    Feat: CORS 처리

--- server/opensource/src/main/java/com/opensource/opensource/controller/ApiController.java
+++ server/opensource/src/main/java/com/opensource/opensource/controller/ApiController.java
@@ -4,6 +4,7 @@ import com.opensource.opensource.dto.VideoInfoDto;
 import com.opensource.opensource.service.RequestTextAnalaysis;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,12 @@ import java.util.ArrayList;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ApiController {
 
     private final RequestTextAnalaysis requestTextAnalaysis;
 
+    @CrossOrigin(origins="*")
     @SneakyThrows
     @GetMapping("/novel")
     public ArrayList<VideoInfoDto> requestResult(@RequestParam String novelUrl, @RequestParam String genre){
