bd6a30188049dff7126dd94ae883e676e0dc20ab
Jun99uu <igun0423@naver.com>
2022-12-03 21:08:26 +0900

    add : 배너 데이터 추가

--- novelism/src/components/Home/HomeCarousel.tsx
+++ novelism/src/components/Home/HomeCarousel.tsx
@@ -1,6 +1,7 @@
 import { Container } from "../../styles/Home/CarouselStyle";
 import CarouselItem from "./HomeCarouselItem";
 import { useState, useEffect, useRef } from "react";
+import { items } from "../../resources/carousel";
 
 interface Item {
   image: string;
@@ -13,42 +14,16 @@ interface Item {
 
 export default function HomeCarousel() {
   const [curIndex, setCurIndex] = useState(0);
-  const dummy_1: Item = {
-    image: "https://i.ytimg.com/vi/dNkUJBF2itI/maxresdefault.jpg",
-    quote: "제국에 대한 애정?\n그런게 내게 남아있던가",
-    tags: ["비극", "긴장", "죽음"],
-    title: "황후를 훔친이는 누구인가 50화",
-    platform: "네이버 웹소설",
-    url: "",
-  };
-  const dummy_2: Item = {
-    image: "https://embed.pixiv.net/artwork.php?illust_id=98244201",
-    quote: "제국에 대한 애정?\n그런게 내게 남아있던가",
-    tags: ["비극", "긴장", "죽음"],
-    title: "황후를 훔친이는 누구인가 50화",
-    platform: "네이버 웹소설",
-    url: "",
-  };
-
-  const dummy_3: Item = {
-    image: "https://cdn.news.unn.net/news/photo/202005/229585_114593_3753.jpg",
-    quote: "삼조에 대한 애정?\n그건 내게 남아있지",
-    tags: ["플러팅", "깔깔", "권모술수"],
-    title: "조용한 자는 말이 없다.",
-    platform: "새내기 배움터",
-    url: "",
-  };
-  const dummies = [dummy_1, dummy_2, dummy_3];
 
   useInterval(() => {
-    setCurIndex((prev) => (prev === dummies.length - 1 ? 0 : prev + 1));
+    setCurIndex((prev) => (prev === items.length - 1 ? 0 : prev + 1));
   }, 7000);
 
   return (
     <Container>
-      <CarouselItem item={dummies[curIndex]} />
+      <CarouselItem item={items[curIndex]} />
       <ul className="carousel-dots">
-        {dummies.map((dummy, index) => (
+        {items.map((dummy, index) => (
           <li
             className={curIndex === index ? "cur-index" : "index"}
             key={`${dummy.title}-${index}`}
---
+++ novelism/src/resources/carousel.ts
@@ -0,0 +1,57 @@
+export interface Item {
+  image: string;
+  quote: string;
+  tags: Array<string>;
+  title: string;
+  platform: string;
+  url: string;
+}
+
+const item1: Item = {
+  image: "https://i.ytimg.com/vi/dNkUJBF2itI/maxresdefault.jpg",
+  quote: "제국에 대한 애정?\n그런게 내게 남아있던가",
+  tags: ["비극", "긴장", "죽음"],
+  title: "황후를 훔친이는 누구인가 50화",
+  platform: "네이버 웹소설",
+  url: "",
+};
+const item2: Item = {
+  image:
+    "https://image.webtoonguide.com/py_c/55/48/7c5f79a17431d0893ae79203fab0",
+  quote: "28번의 전생을,\n모두 기억한다",
+  tags: ["밤", "악몽", "전생"],
+  title: "악마를 배불리는 법 56화",
+  platform: "네이버 웹소설",
+  url: "",
+};
+
+const item3: Item = {
+  image:
+    "https://blog.kakaocdn.net/dn/YqY2k/btrRz1nOKWB/nvC73lCHwa9GH2qtbsL8n0/img.jpg",
+  quote: "나를 죽인 가문으 핏줄로\n다시 태어나다.",
+  tags: ["긴장", "싸움", "배반"],
+  title: "재벌집 막내아들 1화 무료공개",
+  platform: "TOCSODA",
+  url: "",
+};
+
+const item4: Item = {
+  image:
+    "https://tumblbug-pci.imgix.net/e6fefec06fa2e3f92d21a40885f7326b86291ae0/347299b090f677a61ed4215fbfdc9de74a0ffa0a/ae88975640dd3823b9030425c2fa08018f8e724d/488cef47-5e95-412a-a960-006c278586ef.jpeg?ixlib=rb-1.1.0&w=1240&h=930&auto=format%2Ccompress&lossless=true&fit=crop&s=796a9da6c9e7af03e0dbabb7369148be",
+  quote: "화산은,\n사라지지 않는다.",
+  tags: ["꽃", "매화", "미려한"],
+  title: "화산귀환 1406화 독점공개",
+  platform: "네이버 시리즈",
+  url: "",
+};
+
+const item5: Item = {
+  image: "https://pbs.twimg.com/media/Em7PFtGVoAAAX5q.jpg",
+  quote: "창간 9주년\n무료 공개",
+  tags: ["게임", "레벨업", "흥미"],
+  title: "나혼자만 레벨업",
+  platform: "카카오페이지",
+  url: "",
+};
+
+export const items = [item1, item2, item3, item4, item5];
