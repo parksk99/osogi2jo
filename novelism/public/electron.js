d4ffacad98c26d6ffcd7b7015b35660b9cdfef3f
Jun99uu <igun0423@naver.com>
2022-12-04 19:57:34 +0900

    complete : Novelism ver 0.1.0 배포 완료

--- novelism/public/electron.js
+++ novelism/public/electron.js
@@ -4,18 +4,19 @@ var path = require("path");
 var url = require("url");
 var electron_1 = require("electron");
 var isDev = require("electron-is-dev");
+var ipc = electron_1.ipcMain;
 var baseUrl = "http://localhost:3000";
 var mainWindow;
 function createMainWindow() {
     mainWindow = new electron_1.BrowserWindow({
         minWidth: 1080,
-        minHeight: 600,
+        minHeight: 700,
         frame: false,
         webPreferences: {
             nodeIntegration: true,
             webviewTag: true
         },
-        icon: path.join(__dirname, "./512x512.png")
+        icon: path.join(__dirname, "../build/512x512.png")
     });
     var mainWindowUrl = url
         .pathToFileURL(path.join(__dirname, "../build/index.html"))
@@ -23,6 +24,9 @@ function createMainWindow() {
     mainWindow.loadURL(isDev ? baseUrl : mainWindowUrl);
     mainWindow.on("closed", function () {
         mainWindow = null;
+    });
+    ipc.on("closeApp", function () {
+        console.log("닫혀라참깨");
     });
 }
 electron_1.app.on("ready", function () {
